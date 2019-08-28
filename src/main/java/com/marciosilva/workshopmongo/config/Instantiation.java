6843b682ebcf358e97d281a17f8772ca2f6cfd73
silvamarcio <silva.marcio86@hotmail.com>
2019-08-27 23:22:49 -0300

    referenciando os posts do usuario

--- src/main/java/com/marciosilva/workshopmongo/config/Instantiation.java
+++ src/main/java/com/marciosilva/workshopmongo/config/Instantiation.java
@@ -42,9 +42,11 @@ public class Instantiation implements CommandLineRunner{
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou Viajar para São Paulo. Abraços!",new AuthorDto(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"),"Bom dia", "Acordei feliz hoje!", new AuthorDto(maria));
-		
-		
+	
 		postRepository.saveAll(Arrays.asList(post1,post2  ));
+		
+		maria.getPosts().addAll(Arrays.asList(post1,post2));
+		userRepository.save(maria);
 	}
 
 }
